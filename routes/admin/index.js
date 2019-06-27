ad7991aeec401f5c55247f5cbf9cdd651a6cd694
AnshKapoor <ansh605@gmail.com>
2019-06-27 10:38:51 +0530

    Added javascript files for admin

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'admin';
     next();
-})
+});
 router.get('/',(req,res)=>{
     res.render('admin/index');
 });
