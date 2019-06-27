f5c80e289f4150cafd849026f97f7cd313d4d7d0
AnshKapoor <ansh605@gmail.com>
2019-06-27 10:25:56 +0530

    Added css and scss files for the admin page

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbars = require('express-handlebars');
-const main = require('./routes/home/main');
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
 
-app.use('/',main);
+app.use('/',home);
+app.use('/admin',admin);
 app.engine('handlebars',exphbars({defaultLayout:'home'}));
 app.set('view engine','handlebars');
 app.use(express.static(path.join(__dirname,'./public')))
